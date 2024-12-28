af2cd299affdffebded0e8cf6c50d0a00a5a78d3
timealtair <83885014+timealtair@users.noreply.github.com>
2024-12-28 19:08:39 +0400

    impl shorst filtering (hardcode)

--- src/main.js
+++ src/main.js
@@ -1,25 +1,31 @@
-function hideRussianVideos(selectors, title_selectors, excuded_lang_range) {
+function hideRussianVideos(selectors, title_selectors, shorts_selector, shorts_title_attribute, parent_selector) {
     const videoElements = document.querySelectorAll(selectors);
-
     videoElements.forEach(video => {
         const titleElement = video.querySelector(title_selectors);
-        
-        if (titleElement) {
-            if (/[\u0400-\u04FF]/.test(titleElement.textContent)) {
-                video.style.display = 'none';
-            }
+        if (titleElement && /[\u0400-\u04FF]/.test(titleElement.textContent)) {
+            video.style.display = 'none';
+        }
+    });
+
+    const shortsElements = document.querySelectorAll(shorts_selector);
+    shortsElements.forEach(short => {
+        const title = short.getAttribute(shorts_title_attribute);
+        const parentElement = short.closest(parent_selector);
+        if (title && /[\u0400-\u04FF]/.test(title) && parentElement) {
+            parentElement.style.display = 'none';
         }
     });
 }
 
-selectors = 'ytd-rich-item-renderer'
+const selectors = 'ytd-rich-item-renderer';
+const title_selectors = '#video-title';
+const shorts_selector = 'a.shortsLockupViewModelHostEndpoint';
+const shorts_title_attribute = 'title';
+const parent_selector = 'ytd-rich-item-renderer';
 
-title_selectors = '#video-title'
-excuded_lang_range = undefined
+hideRussianVideos(selectors, title_selectors, shorts_selector, shorts_title_attribute, parent_selector);
 
-hideRussianVideos(selectors, title_selectors, excuded_lang_range);
-
-const observer = new MutationObserver(() => hideRussianVideos(selectors,
-                                                              title_selectors,
-                                                              excuded_lang_range));
+const observer = new MutationObserver(() => {
+    hideRussianVideos(selectors, title_selectors, shorts_selector, shorts_title_attribute, parent_selector);
+});
 observer.observe(document.body, { childList: true, subtree: true });
